1808f18b04724264331187e7440355d8d028b544
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-03-28 18:46:38 -0300

    Redux and Saga config and Redux-form components

---
+++ frontend/src/components/FormFields/Input.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Input({
+  input,
+  label,
+  type,
+  htmlFor,
+  disabled,
+  placeholder,
+  meta: { touched, error, warning },
+}) {
+  return (
+    <>
+      {label && <label htmlFor={htmlFor}>{label}</label>}
+      <div>
+        <input
+          {...input}
+          placeholder={placeholder}
+          type={type}
+          disabled={disabled}
+        />
+        {touched &&
+          ((error && <span>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
+      </div>
+    </>
+  );
+}
+
+Input.defaultProps = {
+  label: '',
+  placeholder: '',
+  disabled: false,
+};
+
+Input.propTypes = {
+  input: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  placeholder: PropTypes.string,
+  label: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  htmlFor: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  meta: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
+
+export default Input;
---
+++ frontend/src/components/FormFields/styles/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import Select from 'react-select';
+
+export const MySelector = styled(Select)`
+  width: 198px;
+  padding-bottom: 15px;
+  .react-select__control {
+    margin-top: 4px;
+  }
+  .react-select__value-container {
+    height: 45px;
+  }
+`;
