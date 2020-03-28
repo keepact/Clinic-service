db25cba9dcb512c56e5d9046a743bee2bf9879ae
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-03-28 18:08:11 -0300

    Creating global styles and adding logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 import history from './services/history';
 
@@ -10,6 +12,7 @@ function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
