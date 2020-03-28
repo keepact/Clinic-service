2c7482212803f1d491c684b96505b79f81d97e7f
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-03-28 00:07:33 -0300

    Sequelize, migrations and env variables

---
+++ backend/src/database/migrations/20200328023335-create-schedules.js
@@ -0,0 +1,48 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('schedules', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      specialty_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      professional_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      source_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      birthdate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('schedules');
+  },
+};
