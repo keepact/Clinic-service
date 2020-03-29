24de0ff346c9b9f60c08bee8508a828f67953fa6
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-03-29 14:34:31 -0300

    Specialties page and Header Component

--- frontend/src/pages/Specialties/index.js
+++ frontend/src/pages/Specialties/index.js
@@ -1,7 +1,85 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 
-// import { Container } from './styles';
+import PropTypes from 'prop-types';
 
-export default function Specialties() {
-  return <div />;
+import Header from '~/components/Header';
+import Select from '~/components/FormFields/Select';
+
+import { getProfessionals } from '~/store/ducks/clinic';
+
+import {
+  Container,
+  Content,
+  DoctorsContainer,
+  ButtonContainer,
+  Doctor,
+  Wrapper,
+} from './styles';
+
+function Specialties({ handleSubmit }) {
+  const dispatch = useDispatch();
+
+  const { specialties, professionals } = useSelector((state) => state.clinic);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Content>
+          <h1>Resultados da busca</h1>
+          <form
+            id="Form"
+            onSubmit={handleSubmit((values) =>
+              dispatch(getProfessionals(values.speciality.especialidade_id)),
+            )}>
+            <Field
+              name="speciality"
+              htmlFor="speciality"
+              placeholder="Selecione uma especialidade"
+              options={specialties && specialties}
+              component={Select}
+            />
+          </form>
+        </Content>
+
+        <DoctorsContainer>
+          {professionals?.map((professional) => (
+            <Doctor>
+              <Wrapper>
+                <img
+                  src={
+                    professional.foto
+                      ? professional.foto
+                      : 'https://api.adorable.io/avatars/50/abott@adorable.png'
+                  }
+                  alt="Clinic"
+                />
+                <div>
+                  <h4>{professional.nome.toLowerCase()}</h4>
+                  <span>
+                    {professional.documento_conselho
+                      ? professional.documento_conselho
+                      : 'Não possui CRM cadastrado'}
+                  </span>
+                </div>
+              </Wrapper>
+              <ButtonContainer>
+                <button type="button">Agendar</button>
+              </ButtonContainer>
+            </Doctor>
+          ))}
+        </DoctorsContainer>
+      </Container>
+    </>
+  );
 }
+
+Specialties.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default reduxForm({
+  form: 'SPECIALITY_FORM',
+})(Specialties);
